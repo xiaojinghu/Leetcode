bd6bf9bb9ec2b008e7ea3f41b3114c8d8e9156e4
xiaojinghu <xhu7@buffalo.edu>
2019-08-14 00:58:54 -0400

    add leetcode27 two pointers

---
+++ Leetcode27_2Pointers_Swap.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        # this is a two pointer solution
+        i = 0
+        j = len(nums)-1
+        while(i<=j):
+            if nums[i]==val:
+                nums[i]=nums[j]
+                j -= 1
+            else:
+                i += 1        
+        return j+1

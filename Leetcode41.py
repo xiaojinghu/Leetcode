9f861515b1397e180d6fe4ad92867cfd7ebe5666
xiaojinghu <xhu7@buffalo.edu>
2019-08-14 23:57:34 -0400

    modifeid leetcode 41

--- Leetcode41.py
+++ Leetcode41.py
@@ -7,8 +7,13 @@ class Solution(object):
         # actually we can treat the nums array as a hashtable with indice as key     
         for i in range(len(nums)):
             # for any positive number nums[i], we need to put nums[i] at index nums[nums[i]-1]
+            # Note this is a while loop, after each swap we are sending nums[i] to its correct position
+            # but when we swap we get a new value and it may still not be in the correct position, that's
+            # why we need a while loop. 
             while(nums[i]>0 and nums[i]<=len(nums) and nums[nums[i]-1] != nums[i]):
                 nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+            # after the while loop we are making sure that nums[i]<0(which will either be 
+            # either occupied later or stay vacant) or nums[i] = i+1
         for i in range(len(nums)):
             if nums[i] != i+1:
                 return i+1

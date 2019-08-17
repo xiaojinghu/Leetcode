84ad394fc7f74c604f7da13c69d80ff662693e81
xiaojinghu <xhu7@buffalo.edu>
2019-08-16 21:17:18 -0400

    add leetcode 155 extra stack solution

---
+++ Leetcode155_ExtraStack.py
@@ -0,0 +1,55 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        # the stack is used to store data
+        self.stack = []
+        # the top of the minstack is used to store the minimum so far
+        self.min = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        # if there is no element in the stack, we just add them into the stack and the minstack
+        if not self.stack:
+            self.stack.append(x)
+            self.min.append(x)
+            return
+        self.stack.append(x)
+        self.min.append(min(x, self.min[-1]))
+        return 
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.min.pop()
+        return
+        
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()

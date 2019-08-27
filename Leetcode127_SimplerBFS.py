f465a9d88bfc108e16c606bd55f84758b72afca0
xiaojinghu <xhu7@buffalo.edu>
2019-08-27 16:08:09 -0400

    add leetcode 127 simpler BFS

---
+++ Leetcode127_SimplerBFS.py
@@ -0,0 +1,27 @@
+from collections import deque
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        # this is a simple BFS implementation
+        wordDict = set(wordList+[beginWord])
+        if endWord not in wordDict:
+            return 0
+        
+        queue = deque([(beginWord,1)])
+        while(queue):
+            
+            currWord, currLevel = queue.pop()
+            # pop the word from the dict to avoid repeated visiting
+            if currWord not in wordDict:
+                # it has been visited]
+                continue
+            wordDict.remove(currWord)
+            # check the termination condition
+            if currWord == endWord:
+                return currLevel
+            for i in range(len(currWord)):
+                for j in range(ord('a'), ord('z')+1):
+                    newWord = currWord[:i]+chr(j)+currWord[i+1:]
+                    if newWord in wordDict:
+                        # this means newWord is "connected" with currWord and hasn't been visited
+                        queue.appendleft((newWord, currLevel+1))
+        return 0

ab455f0bce5de18f34ad4bbfca696edc506f26d2
xiaojinghu <xhu7@buffalo.edu>
2019-09-16 03:38:46 -0400

    add leetcode 029 solution

---
+++ Leetcode029.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        # first consider the situation of overflow
+        # case 1
+        if dividend < -2**31:
+            return 2**31-1
+        # case 2
+        if dividend > 2**31-1:
+            return 2**31-1
+        #case 3
+        if dividend == -2**31:
+            if divisor == 1:
+                return -2**31
+            if divisor == -1:
+                return 2**31-1
+        #case 4 
+        if divisor == 1:
+            return dividend
+        if divisor == -1:
+            return -dividend
+        
+        if (dividend > 0 and divisor > 0) or (dividend<0 and divisor<0):
+            sign = 1
+        else:
+            sign = -1
+        divisor = abs(divisor)
+        dividend = abs(dividend)
+        
+        quotient = 0
+        for i in range(31, -1, -1):
+            new_divisor = divisor << i
+            if new_divisor <= dividend:
+                quotient += 1<<i
+                dividend -= new_divisor
+        
+        return sign * quotient
+        

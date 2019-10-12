9174f15fe6dcc150da66109c6446c7308d056e48
xiaojinghu <xhu7@buffalo.edu>
2019-10-12 02:02:01 -0400

    update leetcode 78

--- Leetcode78_backtracking.py
+++ Leetcode78_backtracking.py
@@ -2,14 +2,13 @@ class Solution(object):
     def backtracking(self, nums, i, path, res):
         # temination condition
         if i>= len(nums):
-            return 
-        # Suppose path is an existing solution and now we are at
+            res.append(path)
+            return
+        # Suppose path is an existing solution and now we are at index i
         # we have two choises at position i, add nums[i] to path or not
         
         # case 1: do not add nums[i], build other candidates
         self.backtracking(nums, i+1, path, res)
-        # case 2: add nums[i]
-        res.append(path + [nums[i]])
         # build other candidates
         self.backtracking(nums, i+1, path+[nums[i]], res)
         
@@ -18,6 +17,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        res = [[]]
+        res = []
         self.backtracking(nums, 0, [], res)
         return res

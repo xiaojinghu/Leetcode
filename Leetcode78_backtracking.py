152b497499efaf9a0b66e8319bea7d5dffcf4642
xiaojinghu <xhu7@buffalo.edu>
2019-10-12 01:17:19 -0400

    update leetcode 78

--- Leetcode78_backtracking.py
+++ Leetcode78_backtracking.py
@@ -1,13 +1,17 @@
 class Solution(object):
-    def backtrack(self, nums, i, res, path):
-        if i==len(nums):
+    def backtracking(self, nums, i, path, res):
+        # temination condition
+        if i>= len(nums):
             return 
-        # we add path in the result
-        self.backtrack(nums, i+1, res, path)
-        # we add i into the path
-        res.append(path+[nums[i]])
-        self.backtrack( nums, i+1, res, path+[nums[i]])
+        # Suppose path is an existing solution and now we are at
+        # we have two choises at position i, add nums[i] to path or not
         
+        # case 1: do not add nums[i], build other candidates
+        self.backtracking(nums, i+1, path, res)
+        # case 2: add nums[i]
+        res.append(path + [nums[i]])
+        # build other candidates
+        self.backtracking(nums, i+1, path+[nums[i]], res)
         
     def subsets(self, nums):
         """
@@ -15,5 +19,5 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         res = [[]]
-        self.backtrack(nums, 0, res, [])
+        self.backtracking(nums, 0, [], res)
         return res

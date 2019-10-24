30b3fb8d6014d76bf1d10ab342f59683b60110b9
xiaojinghu <xhu7@buffalo.edu>
2019-10-24 04:35:00 -0400

    2019-10-23 modified leetcode 0004 binary search solution

--- Leetcode0004_BinarySearch.py
+++ Leetcode0004_BinarySearch.py
@@ -6,18 +6,23 @@ class Solution(object):
         # B is always the longer one
         if len(A)>len(B):
             return self.getKth(B, A, k)
+        #Case1: if there is only one valid array which must be B, the we can just return its k-th element 
         if len(A) == 0:
             return B[k-1]
+        #Case2: if we just want the smallest element, then we just return the smaller one among A[0] and B[0]
         if k == 1:
             return min(A[0], B[0])
         # divide A and B both into two parts
         # A[:pa], A[pa:]
-        pa = min(k/2, len(A))
         # B[:pb], B[pb:]
+        # The first two parts of A and B have k elements in total
+        pa = min(k/2, len(A))
         pb = k-pa
         if A[pa-1]<=B[pb-1]:
+            # the biggest element of Apart1 <= the biggest element of Bpart1, this means that the element in Apart1 cannot be the K-th smallest element, we just discard them.
             return self.getKth(A[pa:], B, pb)
         else:
+             # the biggest element of Bpart1 < the biggest element of Apart1, this means that the element in Apart1 cannot be the K-th smallest element, we just discard them.
             return self.getKth(A, B[pb:], pa)
         
     def findMedianSortedArrays(self, nums1, nums2):

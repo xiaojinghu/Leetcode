e74d1b9c2b630f621a94aab9d87ffc0f4987df86
xiaojinghu <xhu7@buffalo.edu>
2019-10-29 16:09:24 -0400

    2019-10-29 add leetcode 141 2 pointers solution

---
+++ Leetcode0141.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head or not head.next:
+            return None
+        
+        slow = head
+        fast = head.next
+        
+        while(slow and fast and fast.next):
+            if slow == fast:
+                return True
+            slow = slow.next
+            fast = fast.next.next
+        return False

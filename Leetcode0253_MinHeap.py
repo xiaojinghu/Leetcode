60f56dbe4d35d0628dc48985ec6c416a353390ae
xiaojinghu <xhu7@buffalo.edu>
2019-10-30 19:28:54 -0400

    update leetcode 253 minheap solution

--- Leetcode0253_MinHeap.py
+++ Leetcode0253_MinHeap.py
@@ -5,22 +5,23 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
-        # sort all the intervals by starting time
+        if not intervals:
+            return 0
+        
+        roomNum = 1
         intervals.sort()
-        roomNum = 0
         
-        # store all the on going meetings in a heap
-        minHeap = []
+        # for each upcoming meeting, we neend only to compare its start time with the previous meeting which ends the earlist, we call it endFirst. If the current meeting starts before endFirst, we will need to open a new room, else can just reuse the rooms assigned before.
+        minHeap = [intervals[0][1]]
         heapq.heapify(minHeap)
-        
-        for i in range(len(intervals)):
+        for i in range(1, len(intervals)):
             startTime, endTime = intervals[i]
-            if not minHeap or startTime<minHeap[0]:
-                # we need to assign a new room for this meeting
-                roomNum += 1
+            if startTime>=minHeap[0]:
+                # we end the previous meeting and start a new meeting. There is no need to spare a new room
+                heapq.heappop(minHeap)
                 heapq.heappush(minHeap, endTime)
                 continue
-            # end current meeting
-            heapq.heappop(minHeap)
+            #b we neend to spare a new room
+            roomNum += 1
             heapq.heappush(minHeap, endTime)
         return roomNum

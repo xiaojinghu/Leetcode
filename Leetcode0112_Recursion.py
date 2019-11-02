21e385041db2c8677a9f0fb60af7ca15034e29ea
xiaojinghu <xhu7@buffalo.edu>
2019-11-02 19:15:25 -0400

    2019-11-02 add leetcode 112 rescursion solution

---
+++ Leetcode0112_Recursion.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return root.val == sum
+        res = False 
+        if root.left:
+            res = res or self.hasPathSum(root.left, sum-root.val)  
+        if root.right:
+            res = res or self.hasPathSum(root.right, sum-root.val)
+        return res
